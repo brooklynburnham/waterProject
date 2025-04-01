284ac209d9c09977ebdc4339cb393ce5242fcd18
brooklynburnham <167155861+brooklynburnham@users.noreply.github.com>
2025-04-01 11:05:59 -0600

    getting ready to commit

--- backend/WaterProject.API/obj/Debug/net8.0/WaterProject.API.AssemblyInfo.cs
+++ backend/WaterProject.API/obj/Debug/net8.0/WaterProject.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95d9cbfbc0624f5540d4cd9cc3548894d77d44cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d89717184fa86af853dbc03dda24757afb62e149")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
